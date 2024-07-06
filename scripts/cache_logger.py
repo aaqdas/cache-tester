31d71c009772587c4219c98376931b68a8210ca5
Ali Aqdas <65295655+aaqdas@users.noreply.github.com>
2024-07-06 17:27:27 +0500

    Changed Directory for Access Log

--- scripts/cache_logger.py
+++ scripts/cache_logger.py
@@ -10,7 +10,7 @@ def log_accesses(filename,cache,physAddr,tags,blockIdx,byteOffset,numberOfWays):
     """
     TAG_FIELD = 1
     VALID_FIELD = 0
-    with open(f"{macros.WRITE_PATH}{filename}.log",'w') as fd:
+    with open(f"{macros.LOG_PATH}{filename}.log",'w') as fd:
         for test_case in range(0,len(physAddr)):
             hit = 0
             for way in range(0,numberOfWays):
@@ -22,7 +22,7 @@ def log_accesses(filename,cache,physAddr,tags,blockIdx,byteOffset,numberOfWays):
                     break
             if not hit:
                 hit = 0
-                fd.write(f"MISS! ; PA: {bin_to_int(physAddr[test_case])}; TAG: {cache[way][TAG_FIELD][blockIdx[test_case]]} ; Accessed Data: 0\n")
+                fd.write(f"MISS! ; PA: {bin_to_int(physAddr[test_case])} ; Accessed Data: 0\n")
                  
              
     
--- scripts/macros.py
+++ scripts/macros.py
@@ -1,4 +1,5 @@
 WRITE_PATH = "../init_files/"
+LOG_PATH = "../log/"
 PHYSICAL_ADDRESS_SZ = 16
 BYTES_PER_WORD = 4
 TEST_CASES = 2**12
